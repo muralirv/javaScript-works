96867e43ff0768aaceaed86a94050ac5cfa3e184
muralirv <145271794+muralirv@users.noreply.github.com>
2024-04-12 20:22:03 +0530

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,37 @@
+/*
+    const a=10;
+    for(var i=0;i<a;i++)
+    console.log(i);
+
+    const b={fname:"murali",age:20};
+    console.log(b);
+    b.fname="venu";
+    console.log(b,typeof b);
+    confirm(b);
+
+    data types:
+    Numbers, Null, Symbols, Objects, Boolean, Undefined, Array, date.
+*/
+
+/*
+        var a=20;
+        var b=true;
+        let c;
+        var d=[20,40,50.23,23,44];
+        var e={
+            fname:"Murali",age:21,email:"1muralirv@gmail.com"
+        }
+        var f=null;
+        console.log(a,typeof a);
+        console.log(b,typeof b);
+        console.log(c,typeof c);
+        console.log(d,typeof d);
+        console.table(e);
+        console.log(f,typeof f);
+
+        var a=Symbol();
+        var b=Symbol();
+        console.log(a,b,a==b); //it will generate unique values
+*/
+var a=new Date();
+console.log(a);
